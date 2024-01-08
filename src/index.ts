bcaf79d89bd96dbe9ef5eefafe97835111f4dc41
Ricardo Chiarelli <ricardo.almeidasantos04@gmail.com>
2024-01-08 19:22:08 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello world");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port!}`);
+});
